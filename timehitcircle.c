6f03c05ecdee161fdd940a6bb87eb9b0dde866e2
Young <christopher.a.young@pleiades.tricity.wsu.edu>
2018-11-11 16:46:41 -0800

    Functional pi estimator and timeing code also. Done whats left is the report

---
+++ timehitcircle.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <limits.h>
+#include <omp.h> 
+#include "dist.c"
+
+int main(int argc, char *argv[]){
+	if (argc < 3){
+		printf("Useage a.out n numthreads\n");
+		return 0;
+	}
+//	printf("test = %lf\n", euclideanDist(0.5,0.5,0.7,0.2));
+	int n;	
+	int numthreads = atoi(argv[2]);
+	omp_set_num_threads(numthreads);
+	if (atoi(argv[1]) == 0)
+		n = INT_MAX;
+	else
+		n = atoi(argv[1]);
+	int hits = 0;
+	int i;
+	srand(time(0));
+	double x,y,cx = 0.5 ,cy = 0.5;
+	double start = omp_get_wtime();	
+	#pragma omp parallel for reduction (+:hits)	
+	for (i=0;i<n;i++){
+			x = ((double)(rand()%1000))/1000;
+			y = ((double)(rand()%1000))/1000;
+		//	printf("(%lf,%lf)\n", x,y);
+		//	printf("dist = %lf\n", euclideanDist(cx,cy,x,y));
+			if (euclideanDist(cx,cy,x,y) < 0.5){
+				hits++;
+			}
+	}
+	double end = omp_get_wtime();
+	printf("hits = %d\n", hits);
+	double pi = 4*((double)hits/(double)n);
+	printf("pi = %.20lf\n",pi );
+	printf("Time for %d loops on %d threads = %.16g\n",n,numthreads,end-start);
+
+
+
+
+
+}
